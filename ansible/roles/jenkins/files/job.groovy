0f49e1922d0c97dc0e8a011ca2ed2f00d79097e3
h0ttab <myemail@example.com>
2026-09-10 21:23:41 +0400

    Extract Job DSL SCM URL into an Ansible variable

--- ansible/roles/jenkins/files/job.groovy
+++ ansible/roles/jenkins/files/job.groovy
@@ -5,19 +5,13 @@ pipelineJob('install-app') {
         numToKeep(5)
         daysToKeep(1)
     }
-    
-    parameters {
-        stringParam('APP_VERSION', '1.0', 'App version')
-        stringParam('APP_PLATFORM', 'linux/amd64', 'App image platform')
-        stringParam('APP_DIR_NAME', 'shareit', 'Application data directory name')
-    }
 
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
-                        url('https://github.com/h0ttab/shareit')
+                        url(System.getenv('APP_SCM_URL'))
                     }
                     branches('main')
                 }
@@ -25,4 +19,6 @@ pipelineJob('install-app') {
             scriptPath('Jenkinsfile')
         }
     }
+
+    queue('install-app')
 }
